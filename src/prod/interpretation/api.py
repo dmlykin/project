9b21dcaf381cee6f2357e5976e588527392f89bc
lykin_da <lykin_da@drom.ru>
2021-03-06 17:27:50 +0700

    swagger

--- src/prod/interpretation/api.py
+++ src/prod/interpretation/api.py
@@ -25,7 +25,7 @@ swagger = Swagger(app)
 
 
 @app.route('/health_check', methods=['GET'])
-@swag_from('colors.yml')
+@swagger.swag_from('colors.yml')
 def health_check():
     data = 'Hello, World!'
     response = app.response_class(
